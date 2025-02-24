6270df489eaab9d52a2f5a958079049e248bd8b6
carolina9191 <valderrama.c.c@gmail.com>
2025-02-24 16:52:43 -0300

    creando primera aplicación en js.

---
+++ app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("El campo está vacío. Por favor, ingrese un nombre válido.");
+        return;
+    }
+    
+    if (amigos.includes(nombre)) {
+        alert("Este dato no es valido");
+        return;
+    }
+    
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach((amigo) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Debe agregar al menos un nombre antes de sortear.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
+}
